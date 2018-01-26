3e324a20dbdb8c1dc9425d1918e9a0cf6c05ce17
SydneyBaxter <sb0973805@RK03-B1225-04.francistuttle.edu>
2018-01-26 14:55:42 -0600

    first file version

--- src/Chapter7/C7_3.java
+++ src/Chapter7/C7_3.java
@@ -1,16 +1,17 @@
-
 package Chapter7;
 
 import java.util.Scanner;
 import java.util.Arrays;
 
 /**
- * asks for the user to input the lengths and elements of two lists, it then compares if they are the same
+ * asks for the user to input the lengths and elements of two lists, it then
+ * compares if they are the same
  *
  * @author Sydney Baxter
  */
 public class C7_3 {
-     /**
+
+    /**
      * Main Method
      *
      * @param args arguments from command line prompt
@@ -19,36 +20,38 @@ public class C7_3 {
         Scanner input = new Scanner(System.in);
         //list 1
         System.out.println("Enter length of list 1");
-        int [] list1 = new int[input.nextInt()];
+        int[] list1 = new int[input.nextInt()];
         //fill the array
         System.out.println("Please enter " + list1.length + " elements");
         for (int x = 0; x < list1.length; x++) {
             list1[x] = input.nextInt();
         }
-        
+
         //list2
-                System.out.println("Enter length of list 2");
-        int [] list2 = new int[input.nextInt()];
+        System.out.println("Enter length of list 2");
+        int[] list2 = new int[input.nextInt()];
         //fill the array
         System.out.println("Please enter " + list2.length + " elements");
         for (int x = 0; x < list2.length; x++) {
             list2[x] = input.nextInt();
         }
-      
-        if(equals(list1, list2)) {
+
+        if (equals(list1, list2)) {
             System.out.println("The 2 lists are identical");
         } else {
             System.out.println("The 2 lists are not identical");
-        }            
+        }
     }
-/**
- * compares the lists
- * @param list1 the elements of list 1
- * @param list2 the elements of list 2
- * @return 
- */
-    public static boolean equals(int[] list1, int [] list2) {
-       
-        return  Arrays.equals(list1, list2); 
+
+    /**
+     * compares the lists
+     *
+     * @param list1 the elements of list 1
+     * @param list2 the elements of list 2
+     * @return if the lists are the same or not
+     */
+    public static boolean equals(int[] list1, int[] list2) {
+
+        return Arrays.equals(list1, list2);
     }
 }
--- src/Chapter7/P7.java
+++ src/Chapter7/P7.java
@@ -1,5 +1,5 @@
-
 package Chapter7;
+
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -10,13 +10,13 @@ import java.util.Arrays;
  * @author Sydney Baxter
  */
 public class P7 {
-    
-      /**
+
+    /**
      * Main Method
      *
      * @param args arguments from command line prompt
-     */       
- public static void main(String[] args) {
+     */
+    public static void main(String[] args) {
         System.out.println("Please enter the array size");
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
@@ -25,41 +25,45 @@ public class P7 {
         System.out.println("Average: " + averager(entered));
         displayArray(entered);
 
-
     }
-/**
- * fills array
- * @param entered amount of entered numbers
- * @param input user input
- */
+
+    /**
+     * fills array
+     *
+     * @param entered amount of entered numbers
+     * @param input user input
+     */
     public static void filler(int[] entered, Scanner input) {
         System.out.println("Please enter" + entered.length + " numbers...");
         for (int x = 0; x < entered.length; x++) {
             entered[x] = input.nextInt();
         }
     }
-/**
- * averages array
- * @param entered entered values
- * @return 
- */
-    public static double averager( int[] entered) {
+
+    /**
+     * averages array
+     *
+     * @param entered entered values
+     * @return the average of the entered numbers
+     */
+    public static double averager(int[] entered) {
 
         Scanner input = new Scanner(System.in);
         double avg = 0;
-        for(int x =0; x < entered.length; ++x){
+        for (int x = 0; x < entered.length; ++x) {
             System.out.println("Enter the value");
-            entered[x]= input.nextInt();
+            entered[x] = input.nextInt();
         }
-        
+
         return avg;
-                }
-/**
- * displays array
- * @param entered entered values
- */
+    }
+
+    /**
+     * displays array
+     *
+     * @param entered entered values
+     */
     public static void displayArray(int[] entered) {
-        System.out.println("Array: " + java.util.Arrays.toString(entered));       
+        System.out.println("Array: " + java.util.Arrays.toString(entered));
     }
 }
-
